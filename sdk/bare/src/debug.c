f509517ba7e2cc6dfd9d6dbf9f90051ee3222491
Nathan Petersen <npetersen2@wisc.edu>
2019-05-20 17:28:47 -0500

    Add debug support by breaking printf into small chunks

--- sdk/bare/src/debug.c
+++ sdk/bare/src/debug.c
@@ -1,15 +1,71 @@
 #include "debug.h"
 #include "scheduler.h"
 #include <stdio.h>
+#include "xil_printf.h"
+#include "defines.h"
+
+
+#define OUTPUT_BUFFER_LENGTH	(4096)
+
+static char output_buffer[OUTPUT_BUFFER_LENGTH];
+static int output_idx = 0;
+
+// Used to mark position in output_buffer of char which needs to output
+// -1 means no output needed
+// >= 0 means print out that idx
+static int print_idx = -1;
 
 void debug_init(void)
 {
 	printf("DB:\tInitializing debug task...\n");
 	scheduler_register_task(debug_callback, DEBUG_INTERVAL_USEC);
-}
 
+	// Clear output buffer
+	for (int i = 0; i < OUTPUT_BUFFER_LENGTH; i++) {
+		output_buffer[i] = 0;
+	}
+}
 
 void debug_callback(void)
 {
-	printf("a");
+	if (print_idx != -1) {
+		// Have work to do!
+
+		char c = output_buffer[print_idx];
+		outbyte(c);
+
+		print_idx++;
+		if (print_idx >= OUTPUT_BUFFER_LENGTH) {
+			print_idx = 0;
+		}
+
+		// Check if done outputting data
+		if (output_buffer[print_idx] == 0) {
+			print_idx = -1;
+		}
+	}
+}
+
+void _append_to_output_buffer(char c)
+{
+	output_buffer[output_idx] = c;
+
+	output_idx++;
+	if (output_idx >= OUTPUT_BUFFER_LENGTH) {
+		output_idx = 0;
+	}
+}
+
+void debug_print(char *msg, int length)
+{
+	// Copy contents into circular output buffer
+
+	print_idx = output_idx;
+
+	for (int i = 0; i < length; i++) {
+		_append_to_output_buffer(msg[i]);
+	}
+
+	// Add NULL termination
+	_append_to_output_buffer(0);
 }
--- sdk/bare/src/debug.h
+++ sdk/bare/src/debug.h
@@ -3,11 +3,13 @@
 
 #include "defines.h"
 
-#define DEBUG_UPDATES_PER_SEC		(1000)
+#define DEBUG_UPDATES_PER_SEC		(300)
 #define DEBUG_INTERVAL_USEC			(USEC_IN_SEC / DEBUG_UPDATES_PER_SEC)
 
 void debug_init(void);
 void debug_callback(void);
+
+void debug_print(char *msg, int length);
 
 
 #endif // DEBUG_H
