6073d4fbacc3ee689553ca9f06a41a21dbbff5da
Nathan Petersen <npetersen2@wisc.edu>
2019-06-17 11:03:14 -0500

    Change inv dtc cmd to accept micro dcomp

--- sdk/bare/usr/params/cmd/cmd_inv.c
+++ sdk/bare/usr/params/cmd/cmd_inv.c
@@ -12,7 +12,7 @@ static command_entry_t cmd_entry;
 
 #define NUM_HELP_ENTRIES	(2)
 static command_help_t cmd_help[NUM_HELP_ENTRIES] = {
-		{"dtc <mDcomp> <mCurrLimit>", "Set dead time compensation"},
+		{"dtc <uDcomp> <mCurrLimit>", "Set deadtime compensation"},
 		{"Vdc <mVdc>", "Set Vdc"}
 };
 
@@ -40,19 +40,19 @@ int cmd_inv(char **argv, int argc)
 		// Check correct number of arguments
 		if (argc != 4) return INVALID_ARGUMENTS;
 
-		// Pull out mDcomp argument
+		// Pull out uDcomp argument
 		// and saturate to 0 .. 0.2
-		double mDcomp = (double) atoi(argv[2]);
-		if (mDcomp > 200.0) return INVALID_ARGUMENTS;
-		if (mDcomp < 0.0) return INVALID_ARGUMENTS;
+		double uDcomp = (double) atoi(argv[2]);
+		if (uDcomp > 200000.0) return INVALID_ARGUMENTS;
+		if (uDcomp < 0.0) return INVALID_ARGUMENTS;
 
 		// Pull out mCurrLimit argument
-		// and saturate to 0 ... 2A
+		// and saturate to 0 ... 4A
 		double mCurrLimit = (double) atoi(argv[3]);
-		if (mCurrLimit > 2000.0) return INVALID_ARGUMENTS;
+		if (mCurrLimit > 4000.0) return INVALID_ARGUMENTS;
 		if (mCurrLimit < 0.0) return INVALID_ARGUMENTS;
 
-		inverter_set_dtc(mDcomp / 1000.0, mCurrLimit / 1000.0);
+		inverter_set_dtc(uDcomp / 1000000.0, mCurrLimit / 1000.0);
 		return SUCCESS;
 	}
 
