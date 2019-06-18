90714d532ddfb89b4911596c3ce48a424712c884
Nathan Petersen <npetersen2@wisc.edu>
2019-06-17 20:54:24 -0500

    Add constant for dq trans to function calls

--- sdk/bare/sys/transform.h
+++ sdk/bare/sys/transform.h
@@ -1,10 +1,13 @@
 #ifndef TRANSFORM_H
 #define TRANSFORM_H
 
-void transform_dqz(double theta, double *abc, double *dqz);
-void transform_dqz_inverse(double theta, double *abc, double *dqz);
+#define TRANS_DQZ_C_INVARIANT_POWER			(0.816496580927726032732) // sqrt(2/3)
+#define TRANS_DQZ_C_INVARIANT_AMPLITUDE		(0.666666666666666666667) // 2/3
 
-void transform_clarke(double *abc, double *xyz);
+void transform_dqz(double C, double theta, double *abc, double *dqz);
+void transform_dqz_inverse(double C, double theta, double *abc, double *dqz);
+
+void transform_clarke(double C, double *abc, double *xyz);
 void transform_park(double theta, double *xyz, double *dqz);
 
 #endif // TRANSFORM_H
