1daa56b146ac0a981752e77682e7afae4c6b1f21
Nathan Petersen <npetersen2@wisc.edu>
2020-05-25 10:21:36 -0500

    Format system commands

--- sdk/bare/common/sys/cmd/cmd_hw.c
+++ sdk/bare/common/sys/cmd/cmd_hw.c
@@ -1,34 +1,30 @@
 #include "cmd_hw.h"
-#include "sys/defines.h"
+#include "drv/analog.h"
+#include "drv/encoder.h"
+#include "drv/pwm.h"
 #include "sys/commands.h"
 #include "sys/debug.h"
-#include "drv/pwm.h"
-#include "drv/encoder.h"
-#include "drv/analog.h"
+#include "sys/defines.h"
 #include <stdint.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 static command_entry_t cmd_entry;
 
-#define NUM_HELP_ENTRIES	(6)
+#define NUM_HELP_ENTRIES (6)
 static command_help_t cmd_help[NUM_HELP_ENTRIES] = {
-    {"pwm sw <freq_switching> <deadtime_ns>", "Set the PWM switching characteristics"},
-    {"pwm duty <pwm_idx> <percent>", "Set a duty ratio"},
-    {"anlg read <chnl_idx>", "Read voltage on ADC channel"},
-    {"enc steps", "Read encoder steps from power-up"},
-    {"enc pos", "Read encoder position"},
-    {"enc init", "Turn on blue LED until Z pulse found"},
+    { "pwm sw <freq_switching> <deadtime_ns>", "Set the PWM switching characteristics" },
+    { "pwm duty <pwm_idx> <percent>", "Set a duty ratio" },
+    { "anlg read <chnl_idx>", "Read voltage on ADC channel" },
+    { "enc steps", "Read encoder steps from power-up" },
+    { "enc pos", "Read encoder position" },
+    { "enc init", "Turn on blue LED until Z pulse found" },
 };
 
 void cmd_hw_register(void)
 {
     // Populate the command entry block
-    commands_cmd_init(&cmd_entry,
-            "hw", "Hardware related commands",
-            cmd_help, NUM_HELP_ENTRIES,
-            cmd_hw
-    );
+    commands_cmd_init(&cmd_entry, "hw", "Hardware related commands", cmd_help, NUM_HELP_ENTRIES, cmd_hw);
 
     // Register the command
     commands_cmd_register(&cmd_entry);
@@ -44,17 +40,22 @@ int cmd_hw(int argc, char **argv)
     if (strcmp("pwm", argv[1]) == 0) {
         if (strcmp("sw", argv[2]) == 0) {
             // Check correct number of arguments
-            if (argc != 5) return INVALID_ARGUMENTS;
+            if (argc != 5)
+                return INVALID_ARGUMENTS;
 
             // Parse out switching freq arg
             double fsw = (double) atoi(argv[3]);
-            if (fsw > 1000000.0) return INVALID_ARGUMENTS;
-            if (fsw < 2000.0) return INVALID_ARGUMENTS;
+            if (fsw > 1000000.0)
+                return INVALID_ARGUMENTS;
+            if (fsw < 2000.0)
+                return INVALID_ARGUMENTS;
 
             // Parse out dead time arg
             int dt = atoi(argv[4]);
-            if (dt > 5000) return INVALID_ARGUMENTS;
-            if (dt < 25) return INVALID_ARGUMENTS;
+            if (dt > 5000)
+                return INVALID_ARGUMENTS;
+            if (dt < 25)
+                return INVALID_ARGUMENTS;
 
             pwm_set_deadtime_ns(dt);
             pwm_set_switching_freq(fsw);
@@ -64,17 +65,22 @@ int cmd_hw(int argc, char **argv)
 
         if (strcmp("duty", argv[2]) == 0) {
             // Check correct number of arguments
-            if (argc != 5) return INVALID_ARGUMENTS;
+            if (argc != 5)
+                return INVALID_ARGUMENTS;
 
             // Parse out switching pwm_idx arg
             int pwm_idx = atoi(argv[3]);
-            if (pwm_idx > 23) return INVALID_ARGUMENTS;
-            if (pwm_idx < 0) return INVALID_ARGUMENTS;
+            if (pwm_idx > 23)
+                return INVALID_ARGUMENTS;
+            if (pwm_idx < 0)
+                return INVALID_ARGUMENTS;
 
             // Parse out percent arg
             int percent = atoi(argv[4]);
-            if (percent > 100) return INVALID_ARGUMENTS;
-            if (percent < 0) return INVALID_ARGUMENTS;
+            if (percent > 100)
+                return INVALID_ARGUMENTS;
+            if (percent < 0)
+                return INVALID_ARGUMENTS;
 
             pwm_set_duty(pwm_idx, (double) percent / 100.0);
 
@@ -86,12 +92,15 @@ int cmd_hw(int argc, char **argv)
     if (strcmp("anlg", argv[1]) == 0) {
         if (strcmp("read", argv[2]) == 0) {
             // Check correct number of arguments
-            if (argc != 4) return INVALID_ARGUMENTS;
+            if (argc != 4)
+                return INVALID_ARGUMENTS;
 
             // Parse out switching pwm_idx arg
             int anlg_idx = atoi(argv[3]);
-            if (anlg_idx > 15) return INVALID_ARGUMENTS;
-            if (anlg_idx < 0) return INVALID_ARGUMENTS;
+            if (anlg_idx > 15)
+                return INVALID_ARGUMENTS;
+            if (anlg_idx < 0)
+                return INVALID_ARGUMENTS;
 
             float value;
             analog_getf(anlg_idx + 1, &value);
@@ -106,7 +115,8 @@ int cmd_hw(int argc, char **argv)
     if (strcmp("enc", argv[1]) == 0) {
         if (strcmp("steps", argv[2]) == 0) {
             // Check correct number of arguments
-            if (argc != 3) return INVALID_ARGUMENTS;
+            if (argc != 3)
+                return INVALID_ARGUMENTS;
 
             int32_t steps;
             encoder_get_steps(&steps);
@@ -118,7 +128,8 @@ int cmd_hw(int argc, char **argv)
 
         if (strcmp("pos", argv[2]) == 0) {
             // Check correct number of arguments
-            if (argc != 3) return INVALID_ARGUMENTS;
+            if (argc != 3)
+                return INVALID_ARGUMENTS;
 
             uint32_t position;
             encoder_get_position(&position);
@@ -130,7 +141,8 @@ int cmd_hw(int argc, char **argv)
 
         if (strcmp("init", argv[2]) == 0) {
             // Check correct number of arguments
-            if (argc != 3) return INVALID_ARGUMENTS;
+            if (argc != 3)
+                return INVALID_ARGUMENTS;
 
             encoder_find_z();
 
--- sdk/bare/common/sys/cmd/cmd_inj.c
+++ sdk/bare/common/sys/cmd/cmd_inj.c
@@ -3,32 +3,28 @@
 #ifndef DISABLE_INJECTION
 
 #include "cmd_inj.h"
-#include "sys/defines.h"
 #include "sys/commands.h"
+#include "sys/defines.h"
 #include "sys/injection.h"
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 static command_entry_t cmd_entry;
 
 #define NUM_HELP_ENTRIES (6)
 static command_help_t cmd_help[NUM_HELP_ENTRIES] = {
-    {"clear", "Clear all injections"},
-    {"list", "List all available injection points"},
-    {"const <name> <set|add|sub> <mValue>", "Inject a constant"},
-    {"noise <name> <set|add|sub> <mGain> <mOffset>", "Inject noise"},
-    {"chirp <name> <set|add|sub> <mGain> <mFreqMin> <mFreqMax> <mPeriod>", "Inject chirp"},
-    {"triangle <name> <set|add|sub> <mValueMin> <mValueMax> <mPeriod>", "Inject triangle"},
+    { "clear", "Clear all injections" },
+    { "list", "List all available injection points" },
+    { "const <name> <set|add|sub> <mValue>", "Inject a constant" },
+    { "noise <name> <set|add|sub> <mGain> <mOffset>", "Inject noise" },
+    { "chirp <name> <set|add|sub> <mGain> <mFreqMin> <mFreqMax> <mPeriod>", "Inject chirp" },
+    { "triangle <name> <set|add|sub> <mValueMin> <mValueMax> <mPeriod>", "Inject triangle" },
 };
 
 void cmd_inj_register(void)
 {
     // Populate the command entry block
-    commands_cmd_init(&cmd_entry,
-            "inj", "Inject signals into system",
-            cmd_help, NUM_HELP_ENTRIES,
-            cmd_inj
-    );
+    commands_cmd_init(&cmd_entry, "inj", "Inject signals into system", cmd_help, NUM_HELP_ENTRIES, cmd_inj);
 
     // Register the command
     commands_cmd_register(&cmd_entry);
@@ -68,17 +64,21 @@ int cmd_inj(int argc, char **argv)
     if (argc == 5 && strcmp("const", argv[1]) == 0) {
         // Parse out name and convert to injection context
         inj_ctx_t *ctx = injection_find_ctx_by_name(argv[2]);
-        if (ctx == NULL) return INVALID_ARGUMENTS;
+        if (ctx == NULL)
+            return INVALID_ARGUMENTS;
 
         // Parse out operation
         inj_op_e op;
-        if (_parse_op(argv[3], &op) != 0) return INVALID_ARGUMENTS;
+        if (_parse_op(argv[3], &op) != 0)
+            return INVALID_ARGUMENTS;
 
         // Pull out mValue argument
         // and saturate to -10 .. 10
         double mValue = (double) atoi(argv[5]);
-        if (mValue < -10000.0) return INVALID_ARGUMENTS;
-        if (mValue >  10000.0) return INVALID_ARGUMENTS;
+        if (mValue < -10000.0)
+            return INVALID_ARGUMENTS;
+        if (mValue > 10000.0)
+            return INVALID_ARGUMENTS;
 
         injection_const(ctx, op, mValue / 1000.0);
 
@@ -89,23 +89,29 @@ int cmd_inj(int argc, char **argv)
     if (argc == 6 && strcmp("noise", argv[1]) == 0) {
         // Parse out name and convert to injection context
         inj_ctx_t *ctx = injection_find_ctx_by_name(argv[2]);
-        if (ctx == NULL) return INVALID_ARGUMENTS;
+        if (ctx == NULL)
+            return INVALID_ARGUMENTS;
 
         // Parse out operation
         inj_op_e op;
-        if (_parse_op(argv[3], &op) != 0) return INVALID_ARGUMENTS;
+        if (_parse_op(argv[3], &op) != 0)
+            return INVALID_ARGUMENTS;
 
         // Pull out mGain argument
         // and saturate to 0 .. 50
         double mGain = (double) atoi(argv[4]);
-        if (mGain < 0.0) return INVALID_ARGUMENTS;
-        if (mGain > 50000.0) return INVALID_ARGUMENTS;
+        if (mGain < 0.0)
+            return INVALID_ARGUMENTS;
+        if (mGain > 50000.0)
+            return INVALID_ARGUMENTS;
 
         // Pull out mOffset argument
         // and saturate to -100 .. 100
         double mOffset = (double) atoi(argv[5]);
-        if (mOffset < -100000.0) return INVALID_ARGUMENTS;
-        if (mOffset >  100000.0) return INVALID_ARGUMENTS;
+        if (mOffset < -100000.0)
+            return INVALID_ARGUMENTS;
+        if (mOffset > 100000.0)
+            return INVALID_ARGUMENTS;
 
         injection_noise(ctx, op, mGain / 1000.0, mOffset / 1000.0);
 
@@ -116,35 +122,45 @@ int cmd_inj(int argc, char **argv)
     if (argc == 8 && strcmp("chirp", argv[1]) == 0) {
         // Parse out name and convert to injection context
         inj_ctx_t *ctx = injection_find_ctx_by_name(argv[2]);
-        if (ctx == NULL) return INVALID_ARGUMENTS;
+        if (ctx == NULL)
+            return INVALID_ARGUMENTS;
 
         // Parse out operation
         inj_op_e op;
-        if (_parse_op(argv[3], &op) != 0) return INVALID_ARGUMENTS;
+        if (_parse_op(argv[3], &op) != 0)
+            return INVALID_ARGUMENTS;
 
         // Pull out mGain argument
         // and saturate to 0 .. 10
         double mGain = (double) atoi(argv[4]);
-        if (mGain < 0.0) return INVALID_ARGUMENTS;
-        if (mGain > 10000.0) return INVALID_ARGUMENTS;
+        if (mGain < 0.0)
+            return INVALID_ARGUMENTS;
+        if (mGain > 10000.0)
+            return INVALID_ARGUMENTS;
 
         // Pull out mFreqMin argument
         // and saturate to 0 .. 10000Hz
         double mFreqMin = (double) atoi(argv[5]);
-        if (mFreqMin < 0.0) return INVALID_ARGUMENTS;
-        if (mFreqMin > 10000000.0) return INVALID_ARGUMENTS;
+        if (mFreqMin < 0.0)
+            return INVALID_ARGUMENTS;
+        if (mFreqMin > 10000000.0)
+            return INVALID_ARGUMENTS;
 
         // Pull out mFreqMax argument
         // and saturate to 0 .. 10000Hz
         double mFreqMax = (double) atoi(argv[6]);
-        if (mFreqMax < 0.0) return INVALID_ARGUMENTS;
-        if (mFreqMax > 10000000.0) return INVALID_ARGUMENTS;
+        if (mFreqMax < 0.0)
+            return INVALID_ARGUMENTS;
+        if (mFreqMax > 10000000.0)
+            return INVALID_ARGUMENTS;
 
         // Pull out mPeriod argument
         // and saturate to 1 .. 60 sec
         double mPeriod = (double) atoi(argv[7]);
-        if (mPeriod < 1000.0) return INVALID_ARGUMENTS;
-        if (mPeriod > 60000.0) return INVALID_ARGUMENTS;
+        if (mPeriod < 1000.0)
+            return INVALID_ARGUMENTS;
+        if (mPeriod > 60000.0)
+            return INVALID_ARGUMENTS;
 
         injection_chirp(ctx, op, mGain / 1000.0, mFreqMin / 1000.0, mFreqMax / 1000.0, mPeriod / 1000.0);
 
@@ -155,29 +171,37 @@ int cmd_inj(int argc, char **argv)
     if (argc == 7 && strcmp("triangle", argv[1]) == 0) {
         // Parse out name and convert to injection context
         inj_ctx_t *ctx = injection_find_ctx_by_name(argv[2]);
-        if (ctx == NULL) return INVALID_ARGUMENTS;
+        if (ctx == NULL)
+            return INVALID_ARGUMENTS;
 
         // Parse out operation
         inj_op_e op;
-        if (_parse_op(argv[3], &op) != 0) return INVALID_ARGUMENTS;
+        if (_parse_op(argv[3], &op) != 0)
+            return INVALID_ARGUMENTS;
 
         // Pull out mValueMin argument
         // and saturate to -100 .. 100
         double mValueMin = (double) atoi(argv[4]);
-        if (mValueMin < -100000.0) return INVALID_ARGUMENTS;
-        if (mValueMin >  100000.0) return INVALID_ARGUMENTS;
+        if (mValueMin < -100000.0)
+            return INVALID_ARGUMENTS;
+        if (mValueMin > 100000.0)
+            return INVALID_ARGUMENTS;
 
         // Pull out mValueMax argument
         // and saturate to -100 .. 100
         double mValueMax = (double) atoi(argv[5]);
-        if (mValueMax < -100000.0) return INVALID_ARGUMENTS;
-        if (mValueMax >  100000.0) return INVALID_ARGUMENTS;
+        if (mValueMax < -100000.0)
+            return INVALID_ARGUMENTS;
+        if (mValueMax > 100000.0)
+            return INVALID_ARGUMENTS;
 
         // Pull out mPeriod argument
         // and saturate to 1 .. 60 sec
         double mPeriod = (double) atoi(argv[6]);
-        if (mPeriod < 1000.0) return INVALID_ARGUMENTS;
-        if (mPeriod > 60000.0) return INVALID_ARGUMENTS;
+        if (mPeriod < 1000.0)
+            return INVALID_ARGUMENTS;
+        if (mPeriod > 60000.0)
+            return INVALID_ARGUMENTS;
 
         injection_triangle(ctx, op, mValueMin / 1000.0, mValueMax / 1000.0, mPeriod / 1000.0);
 
