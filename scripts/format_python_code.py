6e2b0beebf3cc2016f16aac85fce6a529f1d5468
Nick Hemenway <nhemenway@wisc.edu>
2020-07-13 12:03:19 -0500

    add manual configuration file

--- scripts/format_python_code.py
+++ scripts/format_python_code.py
@@ -6,4 +6,4 @@ path = pl.Path('.')
 #loop over all python files and format the files in place
 for file in path.iterdir():
     if file.suffix == '.py' and file.stem != 'format_code':
-        FormatFile(str(file), style_config='pep8', in_place = True)
+        FormatFile(str(file), style_config='setup.cfg', in_place = True)
