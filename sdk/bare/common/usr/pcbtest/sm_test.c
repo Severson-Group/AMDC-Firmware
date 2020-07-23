066a53d002f807394a4900ef89bc114f30a1d54c
Nathan Petersen <npetersen2@wisc.edu>
2020-07-23 16:24:08 -0500

    Add more features to PCB test: config options + smart debug info

--- sdk/bare/common/usr/pcbtest/sm_test.c
+++ sdk/bare/common/usr/pcbtest/sm_test.c
@@ -10,8 +10,9 @@
 #include "usr/user_config.h"
 #include <math.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-static bool test_check_voltage(analog_channel_e channel, float *voltage_out, float goal, float tol);
+static bool test_check_voltage(bool quiet_mode, analog_channel_e channel, float *voltage_out, float goal, float tol);
 
 typedef enum sm_states_e {
     PRINT_HEADER = 0,
@@ -22,9 +23,12 @@ typedef enum sm_states_e {
     TEST1_NEG_10V_RAIL,
 
     TEST2,
-    TEST2_VOUT,
+    TEST2_SET_PWM,
+    TEST2_CHECK_VOUT1,
+    TEST2_CHECK_VOUT2,
 
     END_TESTING,
+    PRINT_SMART_DEBUG,
     REMOVE_TASK,
 } sm_states_e;
 
@@ -33,6 +37,8 @@ typedef struct sm_ctx_t {
     task_control_block_t tcb;
 
     // Top level test config
+    bool quiet_mode;
+    int num_passes;
     int stack;
     char tb;
     float vdrive;
@@ -47,10 +53,15 @@ typedef struct sm_ctx_t {
     int num_passed_tests;
 
     // TEST2 data
-    int curr_vout;
+    int test2_bulk_runs;
+    float curr_pwm_duty_random;
+    float curr_pwm_goal_voltage1;
+    float curr_pwm_goal_voltage2;
+    int curr_pwm;
+    int num_fails_by_vout[6];
 } sm_ctx_t;
 
-#define SM_UPDATES_PER_SEC (1000)
+#define SM_UPDATES_PER_SEC (400)
 #define SM_INTERVAL_USEC   (USEC_IN_SEC / SM_UPDATES_PER_SEC)
 
 static void state_machine_callback(void *arg)
@@ -60,7 +71,9 @@ static void state_machine_callback(void *arg)
     switch (ctx->state) {
     case PRINT_HEADER:
     {
-        debug_printf("Automated PCB Test: PS Port %d%c\r\n", ctx->stack, ctx->tb);
+        debug_printf("Automated PCB Test: Inverter Port %d%c\r\n", ctx->stack, ctx->tb);
+        debug_printf("------------------------------------\r\n");
+        debug_printf("Test started...\r\n");
         ctx->state = TEST1;
         break;
     }
@@ -69,14 +82,24 @@ static void state_machine_callback(void *arg)
     case TEST1_POS_10V_RAIL:
     {
         float voltage;
-        bool pass = test_check_voltage(ANALOG_IN7, &voltage, 10.0, 0.050);
+        bool pass = test_check_voltage(ctx->quiet_mode, ANALOG_IN7, &voltage, 10.0, 0.050);
         if (pass) {
             ctx->num_passed_tests++;
         }
 
-        debug_printf("+10V rail reads %8.4fV\r\n", voltage);
+        if (!ctx->quiet_mode) {
+            debug_printf("+10V rail reads %8.4fV\r\n", voltage);
+        }
 
-        ctx->state = TEST1_NEG_10V_RAIL;
+        if (!pass) {
+            // FAIL ... we can't read >0 voltages, so stop testing!
+            debug_printf("PCB BROKEN: +10V rail read failure. Stopping test.\r\n\n");
+            ctx->state = REMOVE_TASK;
+        } else {
+            // PASS
+            ctx->state = TEST1_NEG_10V_RAIL;
+        }
+
         ctx->num_completed_tests++;
         break;
     }
@@ -84,55 +107,115 @@ static void state_machine_callback(void *arg)
     case TEST1_NEG_10V_RAIL:
     {
         float voltage;
-        bool pass = test_check_voltage(ANALOG_IN8, &voltage, -10.0, 0.050);
+        bool pass = test_check_voltage(ctx->quiet_mode, ANALOG_IN8, &voltage, -10.0, 0.050);
         if (pass) {
             ctx->num_passed_tests++;
         }
 
-        debug_printf("-10V rail reads %8.4fV\r\n", voltage);
+        if (!ctx->quiet_mode) {
+            debug_printf("-10V rail reads %8.4fV\r\n", voltage);
+        }
 
-        ctx->state = TEST2;
+        if (!pass) {
+            // FAIL ... we can't read <0 voltages, so stop testing!
+            debug_printf("PCB BROKEN: -10V rail read failure. Stopping test.\r\n\n");
+            ctx->state = REMOVE_TASK;
+        } else {
+            // PASS
+            ctx->state = TEST2;
+        }
+
         ctx->num_completed_tests++;
         break;
     }
 
     case TEST2:
     {
+        ctx->curr_pwm = 0;
+        ctx->test2_bulk_runs = 0;
+
+        for (int i = 0; i < 6; i++) {
+            ctx->num_fails_by_vout[i] = 0;
+        }
+
+        ctx->state = TEST2_SET_PWM;
+        break;
+    }
+
+    case TEST2_SET_PWM:
+    {
         // Initialize PWM for TEST2
-        int base = 6 * ctx->stack;
+        int base = 6 * (ctx->stack - 1);
         if (ctx->tb == 'B') {
             base += 3;
         }
 
-        pwm_set_duty(base + PWM_OUT1, 0.5);
-        pwm_set_duty(base + PWM_OUT2, 0.5);
-        pwm_set_duty(base + PWM_OUT3, 0.5);
+        // Set PWM output
+        ctx->curr_pwm_duty_random = (float) rand() / (float) RAND_MAX;
+        pwm_set_duty(base + ctx->curr_pwm, ctx->curr_pwm_duty_random);
 
-        ctx->curr_vout = 0;
-        ctx->state = TEST2_VOUT;
+        // Compute goal voltage based on VDRIVE and PWM parameters
+        float pwm_dt = pwm_get_deadtime_ns();
+        float pwm_fsw = pwm_get_switching_freq();
+        float pwm_dt_percent = (((float) pwm_dt) / 1e9) / (1.0 / pwm_fsw);
+        float pwm_dt_delta = pwm_dt_percent * ctx->vdrive;
+
+        float goal1 = (ctx->vdrive * ctx->curr_pwm_duty_random) - pwm_dt_delta;
+        float goal2 = -((ctx->vdrive * (1.0 - ctx->curr_pwm_duty_random)) - pwm_dt_delta);
+        ctx->curr_pwm_goal_voltage1 = goal1;
+        ctx->curr_pwm_goal_voltage2 = goal2;
+
+        ctx->state = TEST2_CHECK_VOUT1;
         break;
     }
 
-    case TEST2_VOUT:
+    case TEST2_CHECK_VOUT1:
     {
-        float goal = 0.5 * ctx->vdrive;
-        if (ctx->curr_vout % 2 == 1) {
-            goal *= -1;
-        }
-
+        int vout = 2 * ctx->curr_pwm;
         float voltage;
-        bool pass = test_check_voltage(ctx->curr_vout, &voltage, goal, 0.100);
+        bool pass = test_check_voltage(ctx->quiet_mode, vout, &voltage, ctx->curr_pwm_goal_voltage1, 0.100);
         if (pass) {
             ctx->num_passed_tests++;
+        } else {
+            ctx->num_fails_by_vout[vout]++;
         }
 
-        debug_printf("VOUT%d: goal = %8.4fV, meas = %8.4f\r\n", ctx->curr_vout, goal, voltage);
+        if (!ctx->quiet_mode) {
+            debug_printf("VOUT%d: goal = %8.4fV, meas = %8.4f\r\n", vout + 1, ctx->curr_pwm_goal_voltage1, voltage);
+        }
 
         ctx->num_completed_tests++;
+        ctx->state = TEST2_CHECK_VOUT2;
+        break;
+    }
 
-        ctx->curr_vout++;
-        if (ctx->curr_vout >= 6) {
-            ctx->state = END_TESTING;
+    case TEST2_CHECK_VOUT2:
+    {
+        int vout = (2 * ctx->curr_pwm) + 1;
+        float voltage;
+        bool pass = test_check_voltage(ctx->quiet_mode, vout, &voltage, ctx->curr_pwm_goal_voltage2, 0.100);
+        if (pass) {
+            ctx->num_passed_tests++;
+        } else {
+            ctx->num_fails_by_vout[vout]++;
+        }
+
+        if (!ctx->quiet_mode) {
+            debug_printf("VOUT%d: goal = %8.4fV, meas = %8.4f\r\n", vout + 1, ctx->curr_pwm_goal_voltage2, voltage);
+        }
+
+        ctx->num_completed_tests++;
+        ctx->state = TEST2_SET_PWM;
+
+        // Update state for next test
+        ctx->curr_pwm++;
+        if (ctx->curr_pwm >= 3) {
+            ctx->curr_pwm = 0;
+            ctx->test2_bulk_runs++;
+
+            if (ctx->test2_bulk_runs >= ctx->num_passes) {
+                ctx->state = END_TESTING;
+            }
         }
 
         break;
@@ -141,7 +224,15 @@ static void state_machine_callback(void *arg)
     case END_TESTING:
     {
         // Print status message to user
-        debug_printf("Done testing: %d of %d tests passed\r\n\n", ctx->num_passed_tests, ctx->num_completed_tests);
+        debug_printf("%d of %d tests passed\r\n", ctx->num_passed_tests, ctx->num_completed_tests);
+
+        if (!ctx->quiet_mode) {
+            for (int i = 0; i < 6; i++) {
+                debug_printf("VOUT%d fails: %d\r\n", i + 1, ctx->num_fails_by_vout[i]);
+            }
+        }
+
+        debug_printf("\n");
 
         // Restore old PWM settings
         pwm_disable();
@@ -149,6 +240,55 @@ static void state_machine_callback(void *arg)
         pwm_set_deadtime_ns(ctx->old_dt);
         if (ctx->old_pwm_en) {
             pwm_enable();
+        }
+
+        ctx->state = PRINT_SMART_DEBUG;
+        break;
+    }
+
+    case PRINT_SMART_DEBUG:
+    {
+        // Give the user some smart debugging info so they don't have to compute this stuff... :)
+        debug_printf("Smart Debug\r\n");
+        debug_printf("-----------\r\n");
+
+        bool any_issues = false;
+        bool pwm_line_is_broken[6] = { 0 };
+
+        // Give info on analog input status
+        for (int i = 0; i < 6; i++) {
+            if (ctx->num_fails_by_vout[i] > ctx->num_passes / 2) {
+                // VOUT is considered broken! > 50% failures
+                debug_printf("ANALOG IN%d may be broken\r\n", i + 1);
+                pwm_line_is_broken[i] = true;
+                any_issues = true;
+            }
+        }
+
+        // Give info on PWM output status
+
+        int rawbase = 12 * (ctx->stack - 1);
+        if (ctx->tb == 'B') {
+            rawbase += 6;
+        }
+
+        int invnum = 2 * (ctx->stack - 1);
+        if (ctx->tb == 'B') {
+            invnum += 1;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            if (pwm_line_is_broken[i]) {
+                // PWM output broken
+                debug_printf("PWM OUT%d may be broken (INV%d_PWM%d)\r\n", rawbase + i + 1, invnum + 1, i + 1);
+            }
+        }
+
+        if (any_issues) {
+            debug_printf("\n");
+            debug_printf("Good luck debugging! :(\r\n\n");
+        } else {
+            debug_printf("No hardware issues found! :)\r\n\n");
         }
 
         ctx->state = REMOVE_TASK;
@@ -166,7 +306,7 @@ static void state_machine_callback(void *arg)
 
 static sm_ctx_t ctx;
 
-int sm_test_start_auto_test(int stack, char tb, double vdrive)
+int sm_test_start_auto_test(bool quiet_mode, int num_passes, int stack, char tb, double vdrive)
 {
     // Ensure test is not already running
     if (scheduler_tcb_is_registered(&ctx.tcb)) {
@@ -181,7 +321,13 @@ int sm_test_start_auto_test(int stack, char tb, double vdrive)
     // Update PWM to as fast as possible
     pwm_disable();
 
-    double fsw = MIN(400e3, PWM_MAX_SWITCHING_FREQ_HZ);
+    // NOTE: as we increase Fsw, the dead-time percentage
+    // increases, so the voltage we see in the tests goes
+    // further from ideal...
+    //
+    // At 300kHz, this is 30x the LPF bandwidth, so we should
+    // see fairly DC at the ADC.
+    double fsw = MIN(300e3, PWM_MAX_SWITCHING_FREQ_HZ);
     if (pwm_set_switching_freq(fsw) != SUCCESS) {
         return FAILURE;
     }
@@ -194,6 +340,8 @@ int sm_test_start_auto_test(int stack, char tb, double vdrive)
 
     // Initialize state machine variables
     ctx.state = PRINT_HEADER;
+    ctx.quiet_mode = quiet_mode;
+    ctx.num_passes = num_passes;
     ctx.stack = stack;
     ctx.tb = tb;
     ctx.vdrive = vdrive;
@@ -209,7 +357,7 @@ int sm_test_start_auto_test(int stack, char tb, double vdrive)
 
 // Helper function
 
-static bool test_check_voltage(analog_channel_e channel, float *voltage_out, float goal, float tol)
+static bool test_check_voltage(bool quiet_mode, analog_channel_e channel, float *voltage_out, float goal, float tol)
 {
     float voltage;
     analog_getf(channel, &voltage);
@@ -221,10 +369,12 @@ static bool test_check_voltage(analog_channel_e channel, float *voltage_out, flo
         is_pass = true;
     }
 
-    if (is_pass) {
-        debug_printf("\tPASS: ");
-    } else {
-        debug_printf("\tFAIL: ");
+    if (!quiet_mode) {
+        if (is_pass) {
+            debug_printf("\tPASS: ");
+        } else {
+            debug_printf("X\tFAIL: ");
+        }
     }
 
     return is_pass;
--- sdk/bare/common/usr/pcbtest/sm_test.h
+++ sdk/bare/common/usr/pcbtest/sm_test.h
@@ -1,6 +1,8 @@
 #ifndef SM_TEST_H
 #define SM_TEST_H
 
-int sm_test_start_auto_test(int stack, char tb, double vdrive);
+#include <stdbool.h>
+
+int sm_test_start_auto_test(bool quiet_mode, int num_passes, int stack, char tb, double vdrive);
 
 #endif // SM_TEST_H
