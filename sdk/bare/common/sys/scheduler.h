39226e27b42f5059583e1b650876598e716b3de9
Nathan Petersen <npetersen2@wisc.edu>
2020-07-26 17:29:25 -0500

    Fix formatting

--- sdk/bare/common/sys/scheduler.h
+++ sdk/bare/common/sys/scheduler.h
@@ -9,7 +9,7 @@
 #include "usr/user_config.h"
 
 // Utility defines for time conversions
-#define USEC_IN_SEC (1000000)
+#define USEC_IN_SEC       (1000000)
 #define SEC_TO_USEC(sec)  (sec * USEC_IN_SEC)
 #define USEC_TO_SEC(usec) (usec / USEC_IN_SEC)
 
