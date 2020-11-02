2be9ce32f807482fad8b3a6679815839296737c0
Ashad Farhan <ashadfarhan@gmail.com>
2020-11-02 15:20:21 -0600

    add enums to define transform type

--- sdk/bare/common/sys/transform.c
+++ sdk/bare/common/sys/transform.c
@@ -2,14 +2,16 @@
 #include "defines.h"
 #include <math.h>
 
-void transform_clarke(double C, double *abc, double *xyz)
+void transform_clarke(transform_dqz_type_e C, double *abc, double *xyz)
 {
-    xyz[0] = C * (1 * abc[0] - 0.5 * abc[1] - 0.5 * abc[2]);
-    xyz[1] = C * (0 * abc[0] + SQRT3 / 2 * abc[1] - SQRT3 / 2 * abc[2]);
-    if (C == TRANS_DQZ_C_INVARIANT_AMPLITUDE) {
+	if (C == TRANS_DQZ_C_INVARIANT_AMPLITUDE) {
+		xyz[0] = C_INVARIANT_AMPLITUDE * (1 * abc[0] - 0.5 * abc[1] - 0.5 * abc[2]);
+    	xyz[1] = C_INVARIANT_AMPLITUDE * (0 * abc[0] + SQRT3 / 2 * abc[1] - SQRT3 / 2 * abc[2]);
     	xyz[2] = (1 / 3 * abc[0] + 1 / 3 * abc[1] + 1 / 3 * abc[2]);
 	}
     if (C == TRANS_DQZ_C_INVARIANT_POWER) {
+		xyz[0] = C_INVARIANT_POWER * (1 * abc[0] - 0.5 * abc[1] - 0.5 * abc[2]);
+    	xyz[1] = C_INVARIANT_POWER * (0 * abc[0] + SQRT3 / 2 * abc[1] - SQRT3 / 2 * abc[2]);
 		xyz[2] = (1 / SQRT3 * abc[0] + 1 / SQRT3 * abc[1] + 1 / SQRT3 * abc[2]);
 	}
 }
@@ -21,14 +23,14 @@ void transform_park(double theta, double *xyz, double *dqz)
     dqz[2] = +0 * xyz[0] + 0 * xyz[1] + 1 * xyz[2];
 }
 
-void transform_dqz(double C, double theta, double *abc, double *dqz)
+void transform_dqz(transform_dqz_type_e C, double theta, double *abc, double *dqz)
 {
     double xyz[3];
     transform_clarke(C, abc, xyz);
     transform_park(theta, xyz, dqz);
 }
 
-void transform_clarke_inverse(double C, double *abc, double *xyz)
+void transform_clarke_inverse(transform_dqz_type_e C, double *abc, double *xyz)
 {
 	if (C == TRANS_DQZ_C_INVARIANT_AMPLITUDE) {
 		abc[0] = 1 * xyz[0] + 0 * xyz[1] + xyz[2];
@@ -36,9 +38,9 @@ void transform_clarke_inverse(double C, double *abc, double *xyz)
 		abc[2] = -1 / 2 * xyz[0] - SQRT3 / 2 * xyz[1] + xyz[2];
 	}
 	if (C == TRANS_DQZ_C_INVARIANT_POWER) {
-		abc[0] = C * (1 * xyz[0] + 0 * xyz[1] + 1 / SQRT2 * xyz[2]);
-		abc[1] = C * (-1 / 2 * xyz[0] + SQRT3 / 2 * xyz[1] + 1 / SQRT2 * xyz[2]);
-		abc[2] = C * (-1 / 2 * xyz[0] - SQRT3 / 2 * xyz[1] + 1 / SQRT2 * xyz[2]);
+		abc[0] = C_INVARIANT_POWER * (1 * xyz[0] + 0 * xyz[1] + 1 / SQRT2 * xyz[2]);
+		abc[1] = C_INVARIANT_POWER * (-1 / 2 * xyz[0] + SQRT3 / 2 * xyz[1] + 1 / SQRT2 * xyz[2]);
+		abc[2] = C_INVARIANT_POWER * (-1 / 2 * xyz[0] - SQRT3 / 2 * xyz[1] + 1 / SQRT2 * xyz[2]);
 	}
 }
 
@@ -48,7 +50,7 @@ void transform_park_inverse(double theta, double *xyz, double *dqz)
 	transform_park(-theta, xyz, dqz);
 }
 
-void transform_dqz_inverse(double C, double theta, double *abc, double *dqz)
+void transform_dqz_inverse(transform_dqz_type_e C, double theta, double *abc, double *dqz)
 {
     double xyz[3];
     transform_park_inverse(theta, xyz, dqz);
