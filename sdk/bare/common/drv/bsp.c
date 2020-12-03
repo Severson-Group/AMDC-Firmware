4a6ac96945f9e42826fa10cf3b08b8733d889c67
BrandtBuchda <bbuchda@wisc.edu>
2020-12-02 18:12:47 -0600

    Added GPIO Mux and drivers

--- sdk/bare/common/drv/bsp.c
+++ sdk/bare/common/drv/bsp.c
@@ -30,8 +30,10 @@
 
 #if USER_CONFIG_HARDWARE_TARGET == AMDC_REV_D
 #include "drv/dac.h"
+#include "drv/gpio_mux.h"
 #include "drv/led.h"
 #include "drv/sts_mux.h"
+
 
 #endif
 
@@ -83,6 +85,7 @@ void bsp_init(void)
 #if USER_CONFIG_HARDWARE_TARGET == AMDC_REV_D
     led_init();
     sts_mux_init();
+    gpio_mux_init();
     dac_init();
 
 #endif
