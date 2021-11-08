c36bf8f6b99f420c65096110c8bc1911a500d658
Nathan Petersen <npetersen2@wisc.edu>
2021-11-08 09:09:26 -0600

    Remove unneeded files; working sockets demo

--- sdk/app_cpu0/src/main.c
+++ sdk/app_cpu0/src/main.c
@@ -3,85 +3,21 @@
 #include "platform.h"
 #include "platform_config.h"
 #include "xil_printf.h"
-#include "lwip/tcp.h"
 #include "xstatus.h"
 #include "xil_cache.h"
 #include "xil_mmu.h"
 #include "xil_io.h"
 #include <stdio.h>
+#include "lwip_glue.h"
 
 /* defined by each RAW mode application */
-void print_app_header();
 int start_application();
 int transfer_data();
 void tcp_fasttmr(void);
 void tcp_slowtmr(void);
 
-/* missing declaration in lwIP */
-void lwip_init();
-
 extern volatile int TcpFastTmrFlag;
 extern volatile int TcpSlowTmrFlag;
-static struct netif server_netif;
-struct netif *echo_netif;
-
-static void print_ip(char *msg, ip_addr_t *ip)
-{
-	print(msg);
-	xil_printf("%d.%d.%d.%d\n\r", ip4_addr1(ip), ip4_addr2(ip), ip4_addr3(ip), ip4_addr4(ip));
-}
-
-static void print_ip_settings(ip_addr_t *ip, ip_addr_t *mask, ip_addr_t *gw)
-{
-	print_ip("Board IP: ", ip);
-	print_ip("Netmask : ", mask);
-	print_ip("Gateway : ", gw);
-}
-
-static int _setup_lwip(void)
-{
-	ip_addr_t ipaddr, netmask, gw;
-
-	// The mac address of the board. This should be unique per board
-	//
-	// => Leave as default from Xilinx example app
-	unsigned char mac_ethernet_address[] = {0x00, 0x0a, 0x35, 0x00, 0x01, 0x02};
-
-	echo_netif = &server_netif;
-
-	/* initliaze IP addresses to be used */
-	IP4_ADDR(&ipaddr,  192, 168,   1, 10);
-	IP4_ADDR(&netmask, 255, 255, 255,  0);
-	IP4_ADDR(&gw,      192, 168,   1,  1);
-
-	print_app_header();
-
-	lwip_init();
-
-	/* Add network interface to the netif_list, and set it as default */
-	if (!xemac_add(echo_netif, &ipaddr, &netmask,
-						&gw, mac_ethernet_address,
-						PLATFORM_EMAC_BASEADDR)) {
-		xil_printf("Error adding N/W interface\n\r");
-		return XST_FAILURE;
-	}
-
-	netif_set_default(echo_netif);
-
-	/* now enable interrupts */
-	platform_enable_interrupts();
-
-	/* specify that the network if is up */
-	netif_set_up(echo_netif);
-
-	print_ip_settings(&ipaddr, &netmask, &gw);
-
-	/* start the application (web server, rxtest, txtest, etc..) */
-	start_application();
-
-	return XST_SUCCESS;
-}
-
 
 int main()
 {
@@ -115,10 +51,28 @@ int main()
 #endif
 
     // Set up LwIP stuff
-    _setup_lwip();
+	print("Setting up LwIP... ");
+    if (setup_lwip() == XST_SUCCESS) {
+		print("Success!\r\n");
+    } else {
+		print("Failure\r\n");
+    	print("Hanging program...\r\n");
+    	while(1);
+    }
+
+	print("Setting up listening sockets... ");
+	if (start_tcpip() == XST_SUCCESS) {
+		print("Success!\r\n");
+	} else {
+		print("Failure\r\n");
+    	print("Hanging program...\r\n");
+    	while(1);
+	}
 
 	// Receive and process packets
 	while (1) {
+		xemacif_input(echo_netif);
+
 		if (TcpFastTmrFlag) {
 			tcp_fasttmr();
 			TcpFastTmrFlag = 0;
@@ -127,8 +81,6 @@ int main()
 			tcp_slowtmr();
 			TcpSlowTmrFlag = 0;
 		}
-		xemacif_input(echo_netif);
-		transfer_data();
 	}
 
 	// Never reached
