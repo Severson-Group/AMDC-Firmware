dbb2718034591d631c5d4c40a14a0e803aa8e764
Nathan Petersen <npetersen2@wisc.edu>
2022-03-09 18:20:43 -0600

    GPIO IP core fix (#246)
    * Add base addr to AMDS drivers
    
    * Update eddy current driver to require base addr
    
    * Denote in drivers that REV D has half the GPIO ports vs REV E
    
    * Update drivers for REV D
    
    * Fix driver function
    
    * Format code

--- sdk/app_cpu1/common/drv/motherboard.c
+++ sdk/app_cpu1/common/drv/motherboard.c
@@ -5,30 +5,35 @@
 #include "drv/motherboard.h"
 #include "drv/motherboard_defs.h"
 #include "sys/cmd/cmd_mb.h"
-#include "sys/debug.h"
+#include "sys/commands.h"
 #include "sys/defines.h"
 #include "sys/util.h"
 #include <stdbool.h>
 #include <stdint.h>
 
-static volatile uint32_t *m_motherboard;
-
-void motherboard_init(uint32_t base_addr)
+void motherboard_init(void)
 {
-    // Store base address for IP
-    m_motherboard = (volatile uint32_t *) base_addr;
-
     cmd_mb_register();
 
 #if USER_CONFIG_ENABLE_MOTHERBOARD_AUTO_TX == 1
-    motherboard_set_adc_sampling(true);
+    bool auto_tx = true;
 #else
-    motherboard_set_adc_sampling(false);
+    bool auto_tx = false
+#endif
+
+    motherboard_set_adc_sampling(MOTHERBOARD_1_BASE_ADDR, auto_tx);
+#if USER_CONFIG_HARDWARE_TARGET == AMDC_REV_E
+    motherboard_set_adc_sampling(MOTHERBOARD_2_BASE_ADDR, auto_tx);
+    motherboard_set_adc_sampling(MOTHERBOARD_3_BASE_ADDR, auto_tx);
+    motherboard_set_adc_sampling(MOTHERBOARD_4_BASE_ADDR, auto_tx);
 #endif
 }
 
-void motherboard_set_adc_sampling(bool enable)
+void motherboard_set_adc_sampling(uint32_t base_addr, bool enable)
 {
+    // Create base address for IP
+    volatile uint32_t *m_motherboard = (volatile uint32_t *) base_addr;
+
     // Read register from FPGA
     uint32_t reg = m_motherboard[MOTHERBOARD_DEFS_OFFSET_CONTROL / 4];
 
@@ -44,8 +49,11 @@ void motherboard_set_adc_sampling(bool enable)
     m_motherboard[MOTHERBOARD_DEFS_OFFSET_CONTROL / 4] = reg;
 }
 
-void motherboard_request_new_data(void)
+void motherboard_request_new_data(uint32_t base_addr)
 {
+    // Create base address for IP
+    volatile uint32_t *m_motherboard = (volatile uint32_t *) base_addr;
+
     // Read register from FPGA
     uint32_t reg = m_motherboard[MOTHERBOARD_DEFS_OFFSET_CONTROL / 4];
 
@@ -56,8 +64,11 @@ void motherboard_request_new_data(void)
     m_motherboard[MOTHERBOARD_DEFS_OFFSET_CONTROL / 4] = reg;
 }
 
-int motherboard_get_data(mb_channel_e channel, int32_t *out)
+int motherboard_get_data(uint32_t base_addr, mb_channel_e channel, int32_t *out)
 {
+    // Create base address for IP
+    volatile uint32_t *m_motherboard = (volatile uint32_t *) base_addr;
+
     if (!motherboard_is_valid_channel(channel)) {
         return FAILURE;
     } else {
@@ -66,19 +77,25 @@ int motherboard_get_data(mb_channel_e channel, int32_t *out)
     }
 }
 
-void motherboard_print_samples(void)
+void motherboard_print_samples(uint32_t base_addr)
 {
+    // Create base address for IP
+    volatile uint32_t *m_motherboard = (volatile uint32_t *) base_addr;
+
     for (int i = 0; i < 8; i++) {
         uint32_t val = m_motherboard[i];
-        debug_printf("%i: %04X\r\n", i, val);
+        cmd_resp_printf("%i: %04X\r\n", i, val);
     }
 }
 
-void motherboard_print_counters(void)
+void motherboard_print_counters(uint32_t base_addr)
 {
-    debug_printf("V: %08X\r\n", m_motherboard[MOTHERBOARD_DEFS_OFFSET_COUNT_VALID / 4]);
-    debug_printf("C: %08X\r\n", m_motherboard[MOTHERBOARD_DEFS_OFFSET_COUNT_CORRUPT / 4]);
-    debug_printf("T: %08X\r\n", m_motherboard[MOTHERBOARD_DEFS_OFFSET_COUNT_TIMEOUT / 4]);
+    // Create base address for IP
+    volatile uint32_t *m_motherboard = (volatile uint32_t *) base_addr;
+
+    cmd_resp_printf("V: %08X\r\n", m_motherboard[MOTHERBOARD_DEFS_OFFSET_COUNT_VALID / 4]);
+    cmd_resp_printf("C: %08X\r\n", m_motherboard[MOTHERBOARD_DEFS_OFFSET_COUNT_CORRUPT / 4]);
+    cmd_resp_printf("T: %08X\r\n", m_motherboard[MOTHERBOARD_DEFS_OFFSET_COUNT_TIMEOUT / 4]);
 }
 
 #endif // USER_CONFIG_ENABLE_MOTHERBOARD_SUPPORT
