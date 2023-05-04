c6a688a845a93a7b1eac5b20194002817e6ca1ef
Patrick Nowakowski <pnowako123@gmail.com>
2023-05-04 12:14:24 -0500

    Restructure gpio_direct_write()

--- sdk/app_cpu1/common/drv/gpio_direct.c
+++ sdk/app_cpu1/common/drv/gpio_direct.c
@@ -92,64 +92,39 @@ gpio_direct_level_t gpio_direct_read(gpio_direct_port_t port, gpio_direct_pin_t 
 
 void gpio_direct_write(gpio_direct_port_t port, gpio_direct_pin_t pin, gpio_direct_level_t level)
 {
+    // Get current register value using Xil_In,
+    // then set the correct bit if write level is HIGH or clear the correct bit if write level is LOW
     uint32_t current = 0;
+    uint32_t base_addr = 0;
 
     switch (port) {
     case GPIO_DIRECT_PORT1:
-        // Get current register value using Xil_In,
-        // then set the correct bit if write level is HIGH
-        // or clear the correct bit if write level is LOW
-
-        current = Xil_In32(GPIO1_DIRECT_BASE_ADDR + sizeof(uint32_t));
-
-        if (level) { // SET
-            Xil_Out32(GPIO1_DIRECT_BASE_ADDR + sizeof(uint32_t),
-                      current | (1 << pin)); // Read register, change only desired bit, write back out
-        } else {                             // CLEAR
-            Xil_Out32(GPIO1_DIRECT_BASE_ADDR + sizeof(uint32_t), current & ~(1 << pin));
-        }
+        base_addr = GPIO1_DIRECT_BASE_ADDR;
         break;
     case GPIO_DIRECT_PORT2:
-        // Get current register value using Xil_In,
-        // then set the correct bit if write level is HIGH
-        // or clear the correct bit if write level is LOW
-
-        current = Xil_In32(GPIO2_DIRECT_BASE_ADDR + sizeof(uint32_t));
-
-        if (level) { // SET
-            Xil_Out32(GPIO2_DIRECT_BASE_ADDR + sizeof(uint32_t), current | (1 << pin));
-        } else { // CLEAR
-            Xil_Out32(GPIO2_DIRECT_BASE_ADDR + sizeof(uint32_t), current & ~(1 << pin));
-        }
+        base_addr = GPIO2_DIRECT_BASE_ADDR;
         break;
 #if USER_CONFIG_HARDWARE_TARGET == AMDC_REV_E
     case GPIO_DIRECT_PORT3:
-        // Get current register value using Xil_In,
-        // then set the correct bit if write level is HIGH
-        // or clear the correct bit if write level is LOW
-
-        current = Xil_In32(GPIO3_DIRECT_BASE_ADDR + sizeof(uint32_t));
-
-        if (level) { // SET
-            Xil_Out32(GPIO3_DIRECT_BASE_ADDR + sizeof(uint32_t), current | (1 << pin));
-        } else { // CLEAR
-            Xil_Out32(GPIO3_DIRECT_BASE_ADDR + sizeof(uint32_t), current & ~(1 << pin));
-        }
+        base_addr = GPIO3_DIRECT_BASE_ADDR;
         break;
     case GPIO_DIRECT_PORT4:
-        // Get current register value using Xil_In,
-        // then set the correct bit if write level is HIGH
-        // or clear the correct bit if write level is LOW
-
-        current = Xil_In32(GPIO4_DIRECT_BASE_ADDR + sizeof(uint32_t));
-
-        if (level) { // SET
-            Xil_Out32(GPIO4_DIRECT_BASE_ADDR + sizeof(uint32_t), current | (1 << pin));
-        } else { // CLEAR
-            Xil_Out32(GPIO4_DIRECT_BASE_ADDR + sizeof(uint32_t), current & ~(1 << pin));
-        }
+        base_addr = GPIO4_DIRECT_BASE_ADDR;
         break;
 #endif
+    }
+
+    // Read the WRITE register, change only desired bit, write back out below
+    // The desired port's WRITE register is the second register in the IP block
+    // Therefore it is at the port's base_addr + sizeof(uint32_t)
+    current = Xil_In32(base_addr + sizeof(uint32_t));
+
+    if (level) { 
+        // SET
+        Xil_Out32(base_addr + sizeof(uint32_t), current | (1 << pin));
+    } else { 
+        // CLEAR
+        Xil_Out32(base_addr + sizeof(uint32_t), current & ~(1 << pin));
     }
 }
 
