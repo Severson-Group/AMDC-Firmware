fafa5ebfd9bdd54ebd9f2a00d55cf81bcfa2c888
lzhao224 <56765259+lzhao224@users.noreply.github.com>
2023-07-13 23:19:58 -0500

    Update solver_rk4_tb_int.sv

--- ip_repo/diff_equation_1.0/src/solver_rk4_tb_int.sv
+++ ip_repo/diff_equation_1.0/src/solver_rk4_tb_int.sv
@@ -1,13 +1,14 @@
 `timescale 1ns / 1ns
 
-module solver_rk4_tb();
+module solver_rk4_tb_int();
 
 reg clk,rst_n;
 reg [63:0] voltage, load;
+wire [63:0] current, speed;
 //////////////////////
 // Instantiate CPU //
 ////////////////////
-solver_rk4_int iDUT(.clk(clk),.rst_n(rst_n), .voltage(voltage), .load(load));
+solver_rk4_int iDUT(.clk(clk),.rst_n(rst_n), .voltage(voltage), .load(load), .i(current), .w(speed));
 initial begin
     clk = 0;
     rst_n = 0;
@@ -17,7 +18,7 @@ initial begin
     repeat(100)@(posedge clk);
 
 	voltage = 64'hFFFFFFFFFFFFFFF;
-	repeat(100000000)@(posedge clk);
+	//repeat(100000000)@(posedge clk);
 	//voltage = 64'h0;
 end
 
