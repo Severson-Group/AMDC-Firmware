e60c19701640e56f57cdbb7a3677bbed6ef884f3
Annika Olson <atolson3@wisc.edu>
2023-07-31 15:04:36 -0500

    Modify encoder driver

--- sdk/app_cpu1/common/drv/encoder.c
+++ sdk/app_cpu1/common/drv/encoder.c
@@ -2,6 +2,7 @@
 #include "sys/defines.h"
 #include "sys/scheduler.h"
 #include "xil_io.h"
+#include <math.h>
 #include <stdio.h>
 
 #define ENCODER_BASE_ADDR (0x43C10000)
@@ -14,17 +15,34 @@ void encoder_init(void)
 
 void encoder_set_pulses_per_rev_bits(uint32_t bits)
 {
-    printf("ENC:\tSetting pulses per rev bits = %ld...\n", bits);
+		printf("ENC:\tSetting pulses per rev bits = %ld...\n", bits);
+	encoder_set_pulses_per_rev(pow((uint32_t) 2), bits));
+}
 
-    Xil_Out32(ENCODER_BASE_ADDR + 2 * sizeof(uint32_t), bits);
+void encoder_set_pulses_per_rev(uint32_t pulses)
+{
+	printf("ENC:\tSetting pulses per rev = %ld...\n", pulses);
+	
+	
+    Xil_Out32(ENCODER_BASE_ADDR + 2 * sizeof(uint32_t), pulses);
 }
 
 void encoder_get_steps(int32_t *steps)
+{
+    *steps = Xil_In32(ENCODER_BASE_ADDR + 2 * sizeof(uint32_t));
+}
+
+void encoder_get_steps_instantaneous(int32_t *steps)
 {
     *steps = Xil_In32(ENCODER_BASE_ADDR);
 }
 
 void encoder_get_position(uint32_t *position)
+{
+	*position = Xil_In32(ENCODER_BASE_ADDR + 3 * sizeof(uint32_t));
+}
+
+void encoder_get_pos_instantaneous(uint32_t *position)
 {
     *position = Xil_In32(ENCODER_BASE_ADDR + 1 * sizeof(uint32_t));
 }
