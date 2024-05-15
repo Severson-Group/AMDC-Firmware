9b41f8d95febc4fc66fccc15975b25301b7b2791
Patrick Nowakowski <pnowako123@gmail.com>
2024-05-15 15:29:54 -0500

    Duplicate and convert "mb" cmd interface to new "amds" cmds

---
+++ sdk/app_cpu1/common/sys/cmd/cmd_amds.c
@@ -0,0 +1,70 @@
+#include "usr/user_config.h"
+
+#if USER_CONFIG_ENABLE_AMDS_SUPPORT == 1
+
+#include "drv/amds.h"
+#include "sys/cmd/cmd_amds.h"
+#include "sys/commands.h"
+#include "sys/util.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+static command_entry_t cmd_entry;
+
+static command_help_t cmd_help[] = {
+    { "<port> data", "Display latest data from all AMDS channels" },
+    { "<port> valid", "Display the validity of all channels' latest data" },
+    { "<port> counters", "Display debug counters from AMDS UART RX" },
+};
+
+void cmd_amds_register(void)
+{
+    commands_cmd_init(&cmd_entry, "amds", "AMDS commands", cmd_help, ARRAY_SIZE(cmd_help), cmd_amds);
+
+    commands_cmd_register(&cmd_entry);
+}
+
+int cmd_amds(int argc, char **argv)
+{
+    uint32_t base_addr = 0;
+
+    if (argc >= 2) {
+        int port = atoi(argv[1]);
+        if (!amds_port_in_bounds(port)) {
+            return CMD_INVALID_ARGUMENTS;
+        }
+
+        base_addr = amds_port_to_base_addr(port);
+    }
+
+    // Handle 'amds <port> data' command
+    if (argc == 3 && STREQ("data", argv[2])) {
+        amds_print_data(base_addr);
+        return CMD_SUCCESS;
+    }
+
+    // Handle 'amds <port> valid' command
+    if (argc == 3 && STREQ("valid", argv[2])) {
+        uint8_t valid_bits = amds_check_data_validity(base_addr);
+
+        for (uint8_t i = 0; i < 8; i++) {
+            if (valid_bits[i])
+                cmd_resp_printf("Channel %i: Valid data", i + 1);
+            else
+                cmd_resp_printf("Channel %i: Invalid data", i + 1);
+        }
+
+        return CMD_SUCCESS;
+    }
+
+    // Handle 'amds <port> counters' command
+    if (argc == 3 && STREQ("counters", argv[2])) {
+        amds_print_counters(base_addr);
+        return CMD_SUCCESS;
+    }
+
+    return CMD_INVALID_ARGUMENTS;
+}
+
+#endif // USER_CONFIG_ENABLE_AMDS_SUPPORT
