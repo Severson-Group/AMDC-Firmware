f6a42d5a09055c9de75873755e3f17fa9e1a9f70
Ryan Srichai <ryansrichai@gmail.com>
2024-12-06 15:30:46 -0600

    updated indentation

--- sdk/app_cpu1/common/drv/timing_manager.c
+++ sdk/app_cpu1/common/drv/timing_manager.c
@@ -445,33 +445,33 @@ double timing_manager_get_time_per_sensor(sensor_e sensor)
  * Get the time since the sensor value was gathered for the requested sensor, in nanoseconds
  */
 double timing_manager_get_time_since_sensor_poll(sensor_e sensor) {
-	uint32_t clock_cycles = Xil_In32(TM_BASE_ADDR + TM_INT_TIME_REG_OFFSET);
-	double time = 0;
+    uint32_t clock_cycles = Xil_In32(TM_BASE_ADDR + TM_INT_TIME_REG_OFFSET);
+    double time = 0;
 
-	if (sensor == ADC)
-		clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_ADC_ENC_TIME_REG_OFFSET)) & TM_LOWER_16_MASK;
-	else if (sensor == ENCODER)
-		clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_ADC_ENC_TIME_REG_OFFSET)) >> TM_UPPER_16_SHIFT;
-	else if (sensor == AMDS_1)
-		clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_AMDS_01_TIME_REG_OFFSET)) & TM_LOWER_16_MASK;
-	else if (sensor == AMDS_2)
-		clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_AMDS_01_TIME_REG_OFFSET)) >> TM_UPPER_16_SHIFT;
-	else if (sensor == AMDS_3)
-		clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_AMDS_23_TIME_REG_OFFSET)) & TM_LOWER_16_MASK;
-	else if (sensor == AMDS_4)
-		clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_AMDS_23_TIME_REG_OFFSET)) >> TM_UPPER_16_SHIFT;
-	else if (sensor == EDDY_1)
-		clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_EDDY_01_TIME_REG_OFFSET)) & TM_LOWER_16_MASK;
-	else if (sensor == EDDY_2)
-		clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_EDDY_01_TIME_REG_OFFSET)) >> TM_UPPER_16_SHIFT;
-	else if (sensor == EDDY_3)
-		clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_EDDY_23_TIME_REG_OFFSET)) & TM_LOWER_16_MASK;
-	else if (sensor == EDDY_4)
-		clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_EDDY_23_TIME_REG_OFFSET)) >> TM_UPPER_16_SHIFT;
+    if (sensor == ADC)
+        clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_ADC_ENC_TIME_REG_OFFSET)) & TM_LOWER_16_MASK;
+    else if (sensor == ENCODER)
+        clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_ADC_ENC_TIME_REG_OFFSET)) >> TM_UPPER_16_SHIFT;
+    else if (sensor == AMDS_1)
+        clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_AMDS_01_TIME_REG_OFFSET)) & TM_LOWER_16_MASK;
+    else if (sensor == AMDS_2)
+        clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_AMDS_01_TIME_REG_OFFSET)) >> TM_UPPER_16_SHIFT;
+    else if (sensor == AMDS_3)
+        clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_AMDS_23_TIME_REG_OFFSET)) & TM_LOWER_16_MASK;
+    else if (sensor == AMDS_4)
+        clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_AMDS_23_TIME_REG_OFFSET)) >> TM_UPPER_16_SHIFT;
+    else if (sensor == EDDY_1)
+        clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_EDDY_01_TIME_REG_OFFSET)) & TM_LOWER_16_MASK;
+    else if (sensor == EDDY_2)
+        clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_EDDY_01_TIME_REG_OFFSET)) >> TM_UPPER_16_SHIFT;
+    else if (sensor == EDDY_3)
+        clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_EDDY_23_TIME_REG_OFFSET)) & TM_LOWER_16_MASK;
+    else if (sensor == EDDY_4)
+        clock_cycles -= (Xil_In32(TM_BASE_ADDR + TM_EDDY_23_TIME_REG_OFFSET)) >> TM_UPPER_16_SHIFT;
 
-	// Convert clock cycles to time in us using 200 MHz FPGA clock frequency
-	time = (double) clock_cycles / CLOCK_FPGA_CLK_FREQ_MHZ;
-	return time;
+    // Convert clock cycles to time in us using 200 MHz FPGA clock frequency
+    time = (double) clock_cycles / CLOCK_FPGA_CLK_FREQ_MHZ;
+    return time;
 }
 
 /*
